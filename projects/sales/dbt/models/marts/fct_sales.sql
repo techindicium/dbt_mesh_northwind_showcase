604194fa08fe0217a72e21586a73d444a81c01af
ivan.fonseca@indicium.tech <ivan.fonseca@indicium.tech>
2026-05-04 14:25:35 +0000

    test

--- projects/sales/dbt/models/marts/fct_sales.sql
+++ projects/sales/dbt/models/marts/fct_sales.sql
@@ -1,6 +1,23 @@
 with
     order_items_metrics as (
-        select *
+        select
+            order_item_sk
+            , product_fk
+            , employee_fk
+            , customer_fk
+            , shipper_fk
+            , order_date
+            , ship_date
+            , required_delivery_date
+            , discount_pct
+            , unit_price
+            , quantity
+            , freight
+            , order_number
+            , recipient_name
+            , recipient_city
+            , recipient_region
+            , recipient_country
         from {{ ref('int_order_items__metrics') }}
     )
 
